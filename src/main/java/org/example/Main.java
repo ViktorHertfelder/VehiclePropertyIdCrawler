8751667a223bd03bf713cd1db3355b47f743309d
viktorhertfelder <viktor.hertfelder@valtech.com>
2023-09-15 14:20:56 +0200

    VehiclePropertyList.csv now conains key-value pairs with all VehicleProperties

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,35 +7,67 @@ import org.jsoup.select.Elements;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.security.Key;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        List<String> propDivList = getDivs();
+
+        Pattern pattern = Pattern.compile("Constant Value:\\s*([0-9]+)");
+
+        HashMap<String, String> propMap = new HashMap<>();
+
+        for (String div : propDivList) {
+            Document document = Jsoup.parse(div);
+            Elements propName = document.select("div[data-version-added] h3.api-name");
+
+            for (Element name : propName) {
+                Element parentDiv = name.parent();
+                String divText = parentDiv.text();
+                Matcher matcher = pattern.matcher(divText);
+                String matchedVal = "";
+
+                if (matcher.find()) {
+                    matchedVal = matcher.group(1);
+                }
+
+                propMap.put(name.text(), matchedVal);
+            }
+        }
+
+        System.out.println(propMap);
+
+        String fileName = "VehiclePropertyList.csv";
+
+        try (FileWriter writer = new FileWriter(fileName)) {
+            for (Map.Entry<String, String> entry : propMap.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                writer.append(key);
+                writer.append(",");
+                writer.append(value);
+                writer.append("\n");
+            }
+        }
+    }
+
+    private static List<String> getDivs() throws IOException {
         String url = "https://developer.android.com/reference/android/car/VehiclePropertyIds#constants_1";
         Document doc = Jsoup.connect(url).get();
 
-        Elements names = doc.select("div[data-version-added] h3");
-        Elements ids = doc.select("div[data-version-added]");
+        Elements propDivs = doc.select("div[data-version-added]");
 
-        List<String> propNameList = new ArrayList<>();
-        for (Element element : names) {
-            String line = element.text();
-            propNameList.add(line);
+        List<String> propDivList = new ArrayList<>();
+        for (Element div : propDivs) {
+            String html = div.html();
+            propDivList.add(html);
         }
-        List<String> propIdList = new ArrayList<>();
-        Pattern pattern = Pattern.compile("Constant Value:\\s*([0-9]+ \\(0x[0-9a-fA-F]+\\))");
-        for (Element id : ids) {
-            String line = id.text();
-            propIdList.add(line);
-            System.out.println(line);
-        }
-
-
-        System.out.println(propNameList.size());
-        System.out.println(propIdList.size());
+        return propDivList;
     }
 }
